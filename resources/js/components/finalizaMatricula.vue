b8dab06e2b8894a567e4e4cdf7996c7efaa5524c
Bruno <brunodiashen@gmail.com>
2021-02-17 20:31:41 -0300

    acentos

--- resources/js/components/finalizaMatricula.vue
+++ resources/js/components/finalizaMatricula.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1 class="mb-5 text-center">Obrigado por matricular-se em nossa escola!</h1>
+        <h1 class="mb-5 text-center">Obrigado por matrícular-se em nossa escola!</h1>
         <div v-for="(info, i) in infos" :key="i">
             <h2>{{ info.title }}</h2>
             <div class="row col-md-10">
@@ -21,7 +21,7 @@ export default {
             img: 'https://cdn.pixabay.com/photo/2019/04/14/10/26/book-4126481_960_720.jpg',
             infos: [{
                     title: 'Iremos avaliar sua matricula.', 
-                    text: 'Nossa escola se empenha para avaliar as matriculas e fazer o melhor para receber você. Esse processo é bastante criterioso. Você receberá o retorno  da situação da sua matrícula e todas as informaçõesnecessárias em três dias, através do email informado. Receberá também a relação dos materiais de compra obrigatória e o valor da mensalidade.'
+                    text: 'Nossa escola se empenha para avaliar as matrículas e fazer o melhor para receber você. Esse processo é bastante criterioso. Você receberá o retorno  da situação da sua matrícula e todas as informaçõesnecessárias em três dias, através do email informado. Receberá também a relação dos materiais de compra obrigatória e o valor da mensalidade.'
                 },{
                     title: 'Contato', 
                     text: 'Telefone: (031) 3281 - 5444 <br>Celular: (031) 9 9999 - 8888 <br>Email: contato@escolinhaavance.com.br <br>Endereço: Av dos Lirios, Nº 1208, Floresta, São Paulo'
